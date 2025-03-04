2caba3a831e82989a358c55fd7ca5fbd6c5c825e
claire <clairemageren@hotmail.com>
2025-03-04 11:31:40 +0100

    data beast and capability

--- src/main/java/be/bstorm/trash/dal/initializers/DataInitializer.java
+++ src/main/java/be/bstorm/trash/dal/initializers/DataInitializer.java
@@ -1,24 +1,65 @@
 package be.bstorm.trash.dal.initializers;
 
+import be.bstorm.trash.dal.repositories.AdoptionRepository;
+import be.bstorm.trash.dal.repositories.BeastRepository;
+import be.bstorm.trash.dal.repositories.CapabilityRepository;
 import be.bstorm.trash.dal.repositories.WizardRepository;
+import be.bstorm.trash.dl.entities.Adoption;
+import be.bstorm.trash.dl.entities.Beast;
+import be.bstorm.trash.dl.entities.Capability;
+import be.bstorm.trash.dl.enums.AdoptionStatus;
+import be.bstorm.trash.dl.enums.DangerLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Set;
 
 @Component
 @RequiredArgsConstructor
 public class DataInitializer implements CommandLineRunner {
 
     private final WizardRepository wizardRepository;
+    private final BeastRepository beastRepository;
+    private final CapabilityRepository capabilityRepository;
+    private final AdoptionRepository adoptionRepository;
 
     @Override
     public void run(String... args) throws Exception {
         saveDefaultWizards();
+        saveDefaultBeasts();
     }
 
     public void saveDefaultWizards() {
         if (wizardRepository.count() == 0) {
             // wizardRepository.saveAll(List.of());
+        }
+    }
+
+    public void saveDefaultBeasts(){
+        if (beastRepository.count() == 0){
+            //Capability
+            Set<Capability> capabilities = Set.of(
+                    new Capability("fire"),
+                    new Capability("frozen"),
+                    new Capability("flight"),
+                    new Capability("small"),
+                    new Capability("big"));
+            capabilityRepository.saveAll(capabilities);
+
+            //Adoption
+            Adoption adoption = new Adoption(AdoptionStatus.PENDING);
+            adoption = adoptionRepository.save(adoption);
+
+            //Beast
+            List<Beast> beasts = List.of(
+                    new Beast("Smaug", true, DangerLevel.INSANE,adoption,capabilities ),
+                    new Beast("Phoenix des Glace", true, DangerLevel.HIGH, adoption, capabilities),
+                    new Beast("Dylan", true, DangerLevel.LOW, adoption, capabilities),
+                    new Beast("Croutard",true,DangerLevel.MEDIUM, adoption, capabilities)
+            );
+            beastRepository.saveAll(beasts);
         }
     }
 }
